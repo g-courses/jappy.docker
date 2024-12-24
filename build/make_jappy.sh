f1210cb1d51acffc40a4e2446a2bcb74b0c7d218
gabriel-astudillo <gabriel.astudillo@gmail.com>
2024-12-24 12:19:51 -0300

    update

--- build/make_jappy.sh
+++ build/make_jappy.sh
@@ -6,7 +6,7 @@ NO_CACHE=--no-cache
 
 case $1 in
     jupyter)
-        docker build $NO_CACHE --platform=$PLATFORM --tag=gastudil/courses:jappy-jupyter -f Dockerfile-base .
+        docker build $NO_CACHE --platform=$PLATFORM --tag=gastudil/courses:jappy-jupyter -f Dockerfile-jupyter .
         ;;
     vscode)
         docker build $NO_CACHE --platform=$PLATFORM --tag=gastudil/courses:jappy-vscode -f Dockerfile-vscode . 
